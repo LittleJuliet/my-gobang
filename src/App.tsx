3b635269ef02cf4bb5c93a2d4cbd2951188d8dff
juliet <luoxianliang@abcyun.cn>
2024-01-05 13:10:26 +0800

    feat: 单机对战

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,65 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
-import './App.scss';
-import {useMount, useUpdateEffect} from "ahooks";
+import React, { useRef, useState } from "react";
+import "./App.scss";
+import { useMount, useUpdateEffect } from "ahooks";
+import { SCORE_ENUM } from "./utils/constant";
+
+const SIZE = 15;
 
 function App() {
-  const [board, setBoard] = useState(Array(15).fill(Array(15).fill(null)));
+  const [board, setBoard] = useState<Array<Array<number>>>(
+    Array(15).fill(Array(15).fill(0)),
+  );
   const canvasRef = React.createRef<HTMLCanvasElement>();
   const toastRef = React.createRef<HTMLDivElement>();
-  const isFinish = useRef<string | null>(null);
+  const isBlack = useRef<1 | 2>(1);
+  const isFinish = useRef<number | null>(null);
   const devicePixelRatio = useRef(window.devicePixelRatio ?? 1);
   const pieceColor = useRef({
-    black: ['#0a0a0a', '#636766'],
-    white: ['#b1b1b1', '#f9f9f9'],
-  })
+    1: ["#0a0a0a", "#636766"],
+    2: ["#b1b1b1", "#f9f9f9"],
+  });
 
+  /**
+   * 玩家落子, 计算是否胜利
+   */
   const handleClick = (row: number, col: number) => {
-    if (board[row][col] === null) {
-      const newBoard = board.map(row => row.slice());
-      newBoard[row][col] = 'black';
+    if (board[row][col] === 0) {
+      const newBoard = board.map((row) => row.slice());
+      newBoard[row][col] = isBlack.current;
       setBoard(newBoard);
-      isFinish.current = checkWinner(newBoard, row, col, 'black');
+      isFinish.current = checkWinner(newBoard, row, col, isBlack.current);
       if (!isFinish.current) {
-        computerMove(newBoard);
+        // computerMove(newBoard);
+        // @ts-ignore
+        isBlack.current = 3 - isBlack.current;
       } else {
         if (isFinish.current && toastRef.current) {
-          toastRef.current.style.display = 'flex';
+          toastRef.current.style.display = "flex";
         }
       }
     }
   };
 
-  // 绘制棋盘
-  const drawBoard = useCallback(() => {
+  /**
+   * 绘制棋盘
+   */
+  const drawBoard = () => {
     if (!canvasRef.current) return;
-    const ctx = canvasRef.current.getContext('2d');
+    const ctx = canvasRef.current.getContext("2d");
     if (!ctx) return;
 
     canvasRef.current.width = 450 * devicePixelRatio.current;
     canvasRef.current.height = 450 * devicePixelRatio.current;
-    canvasRef.current.style.width = '450px';
-    canvasRef.current.style.height = '450px';
+    canvasRef.current.style.width = "450px";
+    canvasRef.current.style.height = "450px";
     ctx.scale(devicePixelRatio.current, devicePixelRatio.current);
 
     // 绘制棋盘背景
-    ctx.fillStyle = 'yellow';
+    ctx.fillStyle = "yellow";
     ctx.fillRect(0, 0, 450, 450);
 
     // 绘制网格线
-    ctx.strokeStyle = 'black';
+    ctx.strokeStyle = "black";
     for (let i = 0; i < 15; i++) {
       // 绘制横线
       ctx.beginPath();
@@ -60,41 +73,67 @@ function App() {
       ctx.lineTo(i * 30 + 15, 435);
       ctx.stroke();
     }
-  }, []);
+  };
 
-  // 绘制棋子
-  const drawPiece = () => {
+  /**
+   * 落子
+   */
+  const drawPiece = (
+    ctx: CanvasRenderingContext2D,
+    i: number,
+    j: number,
+    color: 1 | 2,
+  ) => {
+    ctx.beginPath();
+    ctx.arc(j * 30 + 15, i * 30 + 15, 13, 0, 2 * Math.PI);
+    const gradient = ctx.createRadialGradient(
+      j * 30 + 15,
+      i * 30 + 15,
+      13,
+      j * 30 + 15,
+      i * 30 + 15,
+      0,
+    );
+    gradient.addColorStop(0, pieceColor.current[color][0]);
+    gradient.addColorStop(1, pieceColor.current[color][1]);
+    ctx.fillStyle = gradient;
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  /**
+   * 监听棋盘变化, 绘制棋子
+   */
+  const pieceDrop = () => {
     if (!canvasRef.current) return;
-    const ctx = canvasRef.current.getContext('2d');
+    const ctx = canvasRef.current.getContext("2d");
     if (!ctx) return;
 
     for (let i = 0; i < 15; i++) {
       for (let j = 0; j < 15; j++) {
-        const drawPiece = (color: 'white' | 'black') => {
-          ctx.beginPath();
-          ctx.arc(j * 30 + 15, i * 30 + 15, 13, 0, 2 * Math.PI);
-          const gradient = ctx.createRadialGradient(j * 30 + 15, i * 30 + 15, 13, j * 30 + 15, i * 30 + 15, 0);
-          gradient.addColorStop(0, pieceColor.current[color][0]);
-          gradient.addColorStop(1, pieceColor.current[color][1]);
-          ctx.fillStyle = gradient;
-          ctx.fill();
-          ctx.closePath();
-        };
-        if (board[i][j] === 'black') {
-          drawPiece('black');
-        } else if (board[i][j] === 'white') {
-          drawPiece('white');
+        if (board[i][j] === 1) {
+          drawPiece(ctx, i, j, 1);
+        } else if (board[i][j] === 2) {
+          drawPiece(ctx, i, j, 2);
         }
       }
     }
   };
 
-  const checkWinner = useCallback((newBoard: Array<Array<string | null>>, row: number, col: number, color: string) => {
+  /**
+   * 判断是否有胜利者产生
+   */
+  const checkWinner = (
+    newBoard: Array<Array<number>>,
+    row: number,
+    col: number,
+    color: 1 | 2,
+  ) => {
     const directions = [
       [0, 1], // 横向
       [1, 0], // 纵向
       [1, 1], // 斜向（左上到右下）
-      [1, -1] // 斜向（左下到右上）
+      [1, -1], // 斜向（左下到右上）
     ];
 
     for (const [dx, dy] of directions) {
@@ -105,7 +144,13 @@ function App() {
         const newRow = row + i * dx;
         const newCol = col + i * dy;
 
-        if (newRow >= 0 && newRow < newBoard.length && newCol >= 0 && newCol < newBoard[0].length && newBoard[newRow][newCol] === color) {
+        if (
+          newRow >= 0 &&
+          newRow < newBoard.length &&
+          newCol >= 0 &&
+          newCol < newBoard[0].length &&
+          newBoard[newRow][newCol] === color
+        ) {
           count++;
         } else {
           // 超出边界或不是相同颜色的棋子，停止查找
@@ -118,7 +163,13 @@ function App() {
         const newRow = row - i * dx;
         const newCol = col - i * dy;
 
-        if (newRow >= 0 && newRow < newBoard.length && newCol >= 0 && newCol < newBoard[0].length && newBoard[newRow][newCol] === color) {
+        if (
+          newRow >= 0 &&
+          newRow < newBoard.length &&
+          newCol >= 0 &&
+          newCol < newBoard[0].length &&
+          newBoard[newRow][newCol] === color
+        ) {
           count++;
         } else {
           // 超出边界或不是相同颜色的棋子，停止查找
@@ -134,34 +185,210 @@ function App() {
 
     // 没有获胜
     return null;
-  }, []);
+  };
 
-  const computerMove = useCallback((newBoard: Array<Array<string | null>>) => {
-    const rows = 15, cols = 15;
-    let bestScore = -Infinity;
-    let bestMove = null;
+  /**
+   * 获取指定点的棋子
+   * @param newBoard 当前棋盘
+   * @param point 当前棋子坐标
+   * @param direction 方向 1-左横 2-右横 3-上竖 4-下竖 5-左上斜 6-左下斜 7-右上斜 8-右下斜
+   * @param offset 偏移量
+   * @returns {number} 棋子颜色 -1:越界 0:空 1:黑棋 2:白棋
+   */
+  const relativePoint = (
+    newBoard: Array<Array<number>>,
+    point: { row: number; col: number },
+    direction: number,
+    offset: number,
+  ) => {
+    newBoard = JSON.parse(JSON.stringify(newBoard));
+    let { row, col } = point;
+    switch (direction) {
+      case 1:
+        col -= offset;
+        break;
+      case 2:
+        col += offset;
+        break;
+      case 3:
+        row -= offset;
+        break;
+      case 4:
+        row += offset;
+        break;
+      case 5:
+        col += offset;
+        row -= offset;
+        break;
+      case 6:
+        col -= offset;
+        row += offset;
+        break;
+      case 7:
+        col -= offset;
+        row -= offset;
+        break;
+      case 8:
+        col += offset;
+        row += offset;
+        break;
+    }
+    if (row < 0 || col < 0 || row >= SIZE || col >= SIZE) {
+      return -1;
+    }
+    return newBoard[row][col];
+  };
 
-    // 判断是否是刚开始下棋，如果是，则遍历所有空白位置
-    const isInitialMove = newBoard.every(row => row.every(cell => cell !== 'white'));
+  /**
+   * 拼接各个方向的棋子
+   * @param newBoard 当前棋盘
+   * @param str 棋子字符串
+   * @param point 当前棋子坐标
+   * @param color 当前棋子颜色
+   * @param direction 方向 1-左横 2-右横 3-上竖 4-下竖 5-左上斜 6-左下斜 7-右上斜 8-右下斜
+   * @param offset 偏移量
+   * @returns {string} 拼接后的棋子字符串
+   */
+  const appendPiece = (
+    newBoard: Array<Array<number>>,
+    str: string,
+    point: { row: number; col: number },
+    color: 1 | 2,
+    direction: number,
+    offset: number,
+  ) => {
+    newBoard = JSON.parse(JSON.stringify(newBoard));
+    let piece = relativePoint(newBoard, point, direction, offset);
+    if (piece > -1) {
+      // 如果是黑棋, 需要对黑棋和白棋进行转换, 因为评估枚举是根据白棋设定的
+      if (color === 1) {
+        if (piece > 0) {
+          piece = 3 - piece;
+        }
+      }
+      str += piece;
+    }
+    return str;
+  };
 
-    // 遍历棋盘上所有的白棋位置
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        if (isInitialMove || newBoard[i][j] === 'white') {
-          // 遍历白棋附近的空白位置
-          for (let m = Math.max(0, i - 1); m <= Math.min(rows - 1, i + 1); m++) {
-            for (let n = Math.max(0, j - 1); n <= Math.min(cols - 1, j + 1); n++) {
-              if (newBoard[m][n] === null) {
-                // 尝试在当前空白位置下白棋，并评估得分
-                const score = calcMove(newBoard, m, n, 'white');
+  /**
+   * 获取当前局势
+   * @param newBoard 当前棋盘
+   * @param point 当前棋子坐标
+   * @param direction 方向 1-左横 2-右横 3-上竖 4-下竖 5-左上斜 6-左下斜 7-右上斜 8-右下斜
+   * @param color 当前棋子颜色
+   * @returns {string} 当前局势
+   */
+  const getSituation = (
+    newBoard: Array<Array<number>>,
+    point: { row: number; col: number },
+    direction: number,
+    color: 1 | 2,
+  ) => {
+    newBoard = JSON.parse(JSON.stringify(newBoard));
+    direction = direction * 2 - 1;
+    let str = "";
+    str = appendPiece(newBoard, str, point, color, direction, 4);
+    str = appendPiece(newBoard, str, point, color, direction, 3);
+    str = appendPiece(newBoard, str, point, color, direction, 2);
+    str = appendPiece(newBoard, str, point, color, direction, 1);
+    str += "2";
+    str = appendPiece(newBoard, str, point, color, direction + 1, 1);
+    str = appendPiece(newBoard, str, point, color, direction + 1, 2);
+    str = appendPiece(newBoard, str, point, color, direction + 1, 3);
+    str = appendPiece(newBoard, str, point, color, direction + 1, 4);
+    return str;
+  };
 
-                // 如果得分更高，则更新最佳得分和最佳移动位置
-                if (score > bestScore) {
-                  bestScore = score;
-                  bestMove = { row: m, col: n };
-                }
-              }
-            }
+  /**
+   * 获取当前局势的分数
+   */
+  const getScore = (situation: string) => {
+    Object.keys(SCORE_ENUM).forEach((key) => {
+      if (situation.includes(key)) {
+        // @ts-ignore
+        return SCORE_ENUM[key];
+      }
+    });
+    return 0;
+  };
+
+  const checkSituation = (situation: string, scoreKey: string) => {
+    return situation.includes(scoreKey);
+  };
+
+  /**
+   * 计算电脑落子得分情况
+   */
+  const evaluate = (
+    newBoard: Array<Array<number>>,
+    point: { row: number; col: number },
+    color: 1 | 2,
+  ) => {
+    newBoard = JSON.parse(JSON.stringify(newBoard));
+    let score = 0;
+    let huosanTotal = 0;
+    let chongsiTotal = 0;
+    let huoerTotal = 0;
+    for (let i = 1; i < 5; i++) {
+      const situation = getSituation(newBoard, point, i, color);
+
+      if (
+        checkSituation(situation, "002220") ||
+        checkSituation(situation, "022200") ||
+        checkSituation(situation, "020220") ||
+        checkSituation(situation, "022020")
+      ) {
+        huosanTotal++;
+      } else if (
+        checkSituation(situation, "22220") ||
+        checkSituation(situation, "02222") ||
+        checkSituation(situation, "20222") ||
+        checkSituation(situation, "22022") ||
+        checkSituation(situation, "22202")
+      ) {
+        chongsiTotal++;
+      } else if (
+        checkSituation(situation, "002200") ||
+        checkSituation(situation, "022000") ||
+        checkSituation(situation, "000220")
+      ) {
+        huoerTotal++;
+      }
+
+      score += getScore(situation);
+
+      if (huosanTotal > 0 && huoerTotal > 0) {
+        score *= 2;
+      }
+      if (chongsiTotal > 0 && huoerTotal > 0) {
+        score *= 4;
+      }
+      if (huosanTotal > 1) {
+        score *= 6;
+      }
+      if (chongsiTotal > 0 && huosanTotal > 0) {
+        score *= 8;
+      }
+    }
+    return score;
+  };
+
+  /**
+   * 电脑落子
+   */
+  const computerMove = (newBoard: Array<Array<number>>) => {
+    newBoard = JSON.parse(JSON.stringify(newBoard));
+    let bestMove = { row: -1, col: -1, score: -Infinity };
+
+    for (let i = 0; i < SIZE; i++) {
+      for (let j = 0; j < SIZE; j++) {
+        if (!newBoard[i][j]) {
+          const score =
+            evaluate(newBoard, { row: i, col: j }, 2) +
+            evaluate(newBoard, { row: i, col: j }, 1);
+          if (score > bestMove.score) {
+            bestMove = { row: i, col: j, score };
           }
         }
       }
@@ -169,112 +396,27 @@ function App() {
 
     // 在最佳移动位置下白棋
     if (bestMove) {
-      newBoard[bestMove.row][bestMove.col] = 'white';
+      newBoard[bestMove.row][bestMove.col] = 2;
       setBoard(newBoard);
-      isFinish.current = checkWinner(newBoard, bestMove.row, bestMove.col, 'white');
+      isFinish.current = checkWinner(newBoard, bestMove.row, bestMove.col, 2);
       if (isFinish.current && toastRef.current) {
-        toastRef.current.style.display = 'flex';
+        toastRef.current.style.display = "flex";
       }
     }
-  }, []);
-
+  };
 
   /**
-   * 计算落子的权重
+   * 计算玩家落子坐标
    */
-  const calcMove = useCallback((newBoard: Array<Array<string | null>>, row: number, col: number, color: string) => {
-    const directions = [
-      [0, 1],
-      [1, 0],
-      [1, 1],
-      [1, -1]
-    ];
-
-    let totalScore = 0;
-
-    // 遍历四个方向，评估每个方向的得分
-    for (const [dx, dy] of directions) {
-      const consecutiveColor = countConsecutive(newBoard, row, col, color, dx, dy, 1) +
-          countConsecutive(newBoard, row, col, color, -dx, -dy, 1) - 1;
-      const countColor = countConsecutive(newBoard, row, col, null, dx, dy, 0) +
-          countConsecutive(newBoard, row, col, null, -dx, -dy, 0);
-
-      // 尝试在当前位置下对方的棋
-      const opponentColor = color === 'white' ? 'black' : 'white';
-      const boardCopy = copyBoard(board);
-      boardCopy[row][col] = opponentColor;
-
-      const consecutiveOpponent = countConsecutive(boardCopy, row, col, opponentColor, dx, dy, 1) +
-          countConsecutive(boardCopy, row, col, opponentColor, -dx, -dy, 1) - 1;
-      const countOpponent = countConsecutive(boardCopy, row, col, null, dx, dy, 0) +
-          countConsecutive(boardCopy, row, col, null, -dx, -dy, 0);
-
-      // 计算得分，分别考虑落子方和对方的得分
-      totalScore += calculateScore(consecutiveColor, countColor, color) +
-          calculateScore(consecutiveOpponent, countOpponent, opponentColor);
-    }
-
-    return totalScore;
-  }, []);
-
-  /**
-   * 计算连续的棋子数
-   */
-  const countConsecutive = useCallback((newBoard: Array<Array<string | null>>, row: number, col: number, color: string | null, dx: number, dy: number, count: number) => {
-    while (true) {
-      row += dx;
-      col += dy;
-
-      if (row < 0 || row >= newBoard.length || col < 0 || col >= newBoard[0].length) {
-        break; // 超出边界，停止计数
-      }
-
-      const currentColor = newBoard[row][col];
-      if (currentColor === color || (color === null && currentColor === null)) {
-        count++;
-      } else {
-        break; // 遇到不同颜色的棋子，停止计数
-      }
-    }
-
-    return count;
-  }, []);
-
-  const calculateScore = useCallback((consecutive: number, count: number, color: string) => {
-    if (consecutive >= 5) {
-      return color === 'white' ? 100000 : -100000; // 胜利
-    } else if (consecutive === 4) {
-      if (count === 1) {
-        return color === 'white' ? 1000 : -1000;    // 活四
-      } else if (count === 2) {
-        return color === 'white' ? 100 : -100;      // 死四
-      }
-    } else if (consecutive === 3) {
-      if (count === 2) {
-        return color === 'white' ? 100 : -100;      // 活三
-      } else if (count === 3) {
-        return color === 'white' ? 10 : -10;        // 死三
-      }
-    } else if (consecutive === 2 && count === 3) {
-      return color === 'white' ? 10 : -10;          // 活二
-    }
-
-    return 0;                                       // 无得分
-  }, []);
-
-  // 复制棋盘
-  const copyBoard = useCallback((newBoard: Array<Array<string | null>>) => {
-    return newBoard.map(row => row.slice());
-  }, []);
-
   const playerClickHandler = (e: MouseEvent) => {
     if (isFinish.current) return;
     const rect = (e.target as HTMLCanvasElement).getBoundingClientRect();
-    let x = 0, y = 0;
+    let x = 0,
+      y = 0;
     if (e.clientX - rect.left < 15) {
       x = 0;
     } else if (e.clientX - rect.left > 435) {
-      x = 14
+      x = 14;
     } else {
       x = Math.round((e.clientX - rect.left - 15) / 30);
     }
@@ -288,30 +430,33 @@ function App() {
     handleClick(y, x);
   };
 
+  /**
+   * 初始化绘制棋盘
+   */
   useMount(() => {
     drawBoard();
   });
 
+  /**
+   * 监听棋盘变化, 绘制棋子
+   */
   useUpdateEffect(() => {
-    drawPiece();
+    pieceDrop();
   }, [board]);
 
   return (
-    <div className={'wrapper'}>
-      <div
-          ref={toastRef}
-          className={'toast'}
-      >
-        {`${isFinish.current === 'black' ? '黑方' : '白方'}获胜`}
+    <div className={"wrapper"}>
+      <div ref={toastRef} className={"toast"}>
+        {`${isFinish.current === 1 ? "黑方" : "白方"}获胜`}
       </div>
       <canvas
-          ref={canvasRef}
-          width={450}
-          height={450}
-          onClick={(e) => {
-            // @ts-ignore
-            playerClickHandler(e as MouseEvent);
-          }}
+        ref={canvasRef}
+        width={450}
+        height={450}
+        onClick={(e) => {
+          // @ts-ignore
+          playerClickHandler(e as MouseEvent);
+        }}
       ></canvas>
     </div>
   );
